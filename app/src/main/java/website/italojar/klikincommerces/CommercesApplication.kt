954f0e59ff14f6f0382eb8180701d28e5e9d6060
italojar <jaramilloarmijositalo@gmail.com>
2022-09-05 20:23:32 +0200

    Added Dagger Hilt

---
+++ app/src/main/java/website/italojar/klikincommerces/CommercesApplication.kt
@@ -0,0 +1,7 @@
+package website.italojar.klikincommerces
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CommercesApplication: Application()
--- app/src/main/java/website/italojar/klikincommerces/presentation/MainActivity.kt
+++ app/src/main/java/website/italojar/klikincommerces/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package website.italojar.klikincommerces.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import website.italojar.klikincommerces.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
